195bfa0576862c9529e71c087b3b60d1cb67c4bb
Arthur-codeX <daniel.karanja239@gmail.com>
2025-07-10 10:16:50 +0300

    Object Data

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import AllData from "./DisplayData/AllData";
+import ObjData from "./DisplayData/ObjData";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AllData />
+    <ObjData />
   </StrictMode>
 );
