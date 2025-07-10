96cbc2b98da711b728dc0a75dcb549f23bb122d8
Arthur-codeX <daniel.karanja239@gmail.com>
2025-07-10 09:28:45 +0300

    updated display data

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import CatsAndDogs from "./Exports/MyFile";
-
-// {}->NAME IS REQUIRED
-// Destructure
-import { Component3, Component2 } from "./Exports/NamedExports";
-
-import MainComponent, { C1, C2 } from "./Exports/CombinedExport";
+import AllData from "./DisplayData/AllData";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainComponent />
-    <C1 />
-    <C2 />
+    <AllData />
   </StrictMode>
 );
